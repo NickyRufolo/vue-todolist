f5f3e5e20c285377915f60b392d6e50dc6dffeab
NikiRufolo <nikirufolo@gmail.com>
2022-11-14 17:33:43 +0100

    vue js

--- main.js
+++ main.js
@@ -3,24 +3,40 @@ var app = new Vue({
     data: {
         todoList:[
         {
-            testo: `Imparare HTML`
+            testo: `Imparare HTML`,
+            done : false
         },
         {
-            testo: `Imparare CSS`
+            testo: `Imparare CSS`,
+            done : false
         },
         {
-            testo: `Imparare Javascript`
+            testo: `Imparare Javascript`,
+            done : false
         },
         {
-            testo: `Imparare PHP`
+            testo: `Imparare PHP`,
+            done : false
         },
         {
-            testo: `Imparare Laravel`
+            testo: `Imparare Laravel`,
+            done : false
         }
-    ]
+    ],
+    taskCompleted: []
     },
+    value:``,
     methods: {
+        TodoLista(indice){
+            //per rimuovere oggetti all'array uso splice() dove il parametro sarà l'indexcliccato di riferimento
+            this.todoList.splice(indice, 1); //quanti elmenti eliminare cioe 1 
+            this.taskCompleted.push(indice)
+        },
         
+        inputTask() {
+            let inputField = document.getElementById(`InputUtente`).value;
+            this.todoList.push({testo: `${inputField}`,done : false})
+        }
     }
 })
 

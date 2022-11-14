2ea11b796d0530cf4ef3927bc189b913106c4b7b
NikiRufolo <nikirufolo@gmail.com>
2022-11-14 15:15:10 +0100

    todo list starting

---
+++ main.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        todoList:[
+        {
+            testo: `Imparare HTML`
+        },
+        {
+            testo: `Imparare CSS`
+        },
+        {
+            testo: `Imparare Javascript`
+        },
+        {
+            testo: `Imparare PHP`
+        },
+        {
+            testo: `Imparare Laravel`
+        }
+    ]
+    },
+    methods: {
+        
+    }
+})
+
+
+
+
+
